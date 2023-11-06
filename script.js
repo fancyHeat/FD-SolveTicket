e350f03dba14e27cfb5247121de83f4c6793d633
fancyHeat <shae@notics.io>
2023-11-06 17:49:33 -0600

    JS improvements

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
+'use strict';
+
+const RECAPTCHA_SITE_KEY = '6LdR6JMoAAAAAARPee2wQpRnTrJaEI9xvxcb2pH6';
+const WEBHOOK_URL = 'https://hook.us1.make.com/kzamtepys7dyjrjv2bt7ssj7iwlmfrst';
+
+document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const code = removeZeroWidthSpace(decodeURIComponent(urlParams.get('code')));
-    const ticket = removeZeroWidthSpace(decodeURIComponent(urlParams.get('ticket')));
 
     // Check if both code and ticket parameters are present
-    if (code && ticket) {
+    if (urlParams.has('code') && urlParams.has('ticket')) {
+        const code = removeZeroWidthSpace(decodeURIComponent(urlParams.get('code')));
+        const ticket = removeZeroWidthSpace(decodeURIComponent(urlParams.get('ticket')));
+
         // Call the reCAPTCHA API
         grecaptcha.ready(function () {
-            // Replace 'YOUR_SITE_KEY' with your actual reCAPTCHA v3 site key
-            grecaptcha.execute('6LdR6JMoAAAAAARPee2wQpRnTrJaEI9xvxcb2pH6')
+            grecaptcha.execute(RECAPTCHA_SITE_KEY)
                 .then(function (token) {
                     // Handle the token as needed (e.g., send it to the server for verification)
                     sendToWebhook({ "token": token, "code": code, "ticket": ticket });
+                })
+                .catch(function (error) {
+                    console.error('Error executing server api call:', error);
                 });
         });
     } else {
         const messageContainer = document.getElementById('message-container');
-        if (messageContainer) {
-            messageContainer.innerHTML = 'Code and ticket parameters are required in the URL.';
+        if (messageContainer !== null) {
+            messageContainer.innerHTML = 'Both code and ticket parameters are required in the URL.';
         }
-        console.error('Code and ticket parameters are required in the URL.');
+        console.error('Both code and ticket parameters are required in the URL.');
     }
 });
 
@@ -28,26 +36,24 @@ function removeZeroWidthSpace(inputString) {
 }
 
 function sendToWebhook(data) {
-    const webhookUrl = 'https://hook.us1.make.com/kzamtepys7dyjrjv2bt7ssj7iwlmfrst';
-
-    fetch(webhookUrl, {
+    fetch(WEBHOOK_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(responseData => {
-        console.log('Webhook response:', responseData);
+        .then(response => response.json())
+        .then(responseData => {
+            console.log('Webhook response:', responseData);
 
-        const messageContainer = document.getElementById('message-container');
-        if (messageContainer) {
-            const msgValue = responseData.msg || 'No message available';
-            messageContainer.innerHTML = `${msgValue}`;
-        }
-    })
-    .catch(error => {
-        console.error('Error sending token to webhook:', error);
-    });
+            const messageContainer = document.getElementById('message-container');
+            if (messageContainer !== null) {
+                const msgValue = responseData.msg || 'No message available';
+                messageContainer.innerHTML = `${msgValue}`;
+            }
+        })
+        .catch(error => {
+            console.error('Error sending token to webhook:', error);
+        });
 }
