184b5ca75973b6e65e1305b9bc4c492df554ccbe
fancyHeat <shae@notics.io>
2023-11-06 16:56:26 -0600

    Added Query Parameter checks

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-// Execute this function once all the page content is loaded
 document.addEventListener("DOMContentLoaded", function () {
-
     const urlParams = new URLSearchParams(window.location.search);
     const code = decodeURIComponent(urlParams.get('code'));
     const ticket = decodeURIComponent(urlParams.get('ticket'));
-    
-    // Call the reCAPTCHA API
-    grecaptcha.ready(function () {
-        // Replace 'YOUR_SITE_KEY' with your actual reCAPTCHA v3 site key
-        grecaptcha.execute('6LdR6JMoAAAAAARPee2wQpRnTrJaEI9xvxcb2pH6')
-            .then(function (token) {
-                // Handle the token as needed (e.g., send it to the server for verification)
-                sendToWebhook({ "token": token, "code": code, "ticket": ticket });
-            });
-    });
+
+    // Check if both code and ticket parameters are present
+    if (code && ticket) {
+        // Call the reCAPTCHA API
+        grecaptcha.ready(function () {
+            // Replace 'YOUR_SITE_KEY' with your actual reCAPTCHA v3 site key
+            grecaptcha.execute('6LdR6JMoAAAAAARPee2wQpRnTrJaEI9xvxcb2pH6')
+                .then(function (token) {
+                    // Handle the token as needed (e.g., send it to the server for verification)
+                    sendToWebhook({ "token": token, "code": code, "ticket": ticket });
+                });
+        });
+    } else {
+        const messageContainer = document.getElementById('message-container');
+        if (messageContainer) {
+            messageContainer.innerHTML = 'Code and ticket parameters are required in the URL.';
+        }
+        console.error('Code and ticket parameters are required in the URL.');
+    }
 });
+
 function sendToWebhook(data) {
-    // Replace 'YOUR_WEBHOOK_URL' with the actual URL of your webhook
     const webhookUrl = 'https://hook.us1.make.com/kzamtepys7dyjrjv2bt7ssj7iwlmfrst';
 
-    // Make a POST request to the webhook
     fetch(webhookUrl, {
         method: 'POST',
         headers: {
@@ -29,7 +35,6 @@ function sendToWebhook(data) {
     })
     .then(response => response.json())
     .then(responseData => {
-        // Handle the response from the server if needed
         console.log('Webhook response:', responseData);
 
         const messageContainer = document.getElementById('message-container');
@@ -42,4 +47,3 @@ function sendToWebhook(data) {
         console.error('Error sending token to webhook:', error);
     });
 }
-
